676623eccca6e546c5708cfe70285d71373e85cf
Eric Z Chen <chvlyl@gmail.com>
2015-10-03 17:56:12 -0400

    re-write ziw function

--- R/zi_wilcox_test.R
+++ R/zi_wilcox_test.R
@@ -1,64 +1,70 @@
-#' Modified Wilcoxon rank test for zero-inflated data
+#' Modified Wilcoxon rank test (ZIW) for zero-inflated data
 #'
-#' @param x the covariates for zero proportion
-#' @param y the covariates for non-zero compoment
-#' @param alpha the significant level
-#' @param perm use permutation
-#' @return pvalue
+#' @param x a vector of data for one group
+#' @param y a vector of data for another group
+#' @param perm use permutation to calculate the pvalue
+#' @return modified wilcoxon rank sum statistic and pvalue
 #' @export
 #' @examples
-#' zi_wilcox_test(x, y, alpha = 0.05, perm = FALSE)
+#' x <- c(rep(0,5),rlnorm(20, meanlog = 0, sdlog = 1))
+#' y <- c(rep(0,10),rlnorm(20, meanlog = 2, sdlog = 1))
+#' ziw(x, y, perm = FALSE)
+#' ziw(x, y, perm = TRUE)
 
 
-
-zi_wilcox_test <- function(x, y, alpha = 0.05, perm = FALSE) {
-	
-	N = c(length(x), length(y));
-
-	###########Our Test ###################
-	n = c(sum(x != 0), sum(y != 0));
-	prop = n/N; pmax = max(prop); pmean = mean(prop);
-	Ntrun = round(pmax*N);
-	Xtrun = c(x[x != 0], rep(0, Ntrun[1] - n[1]));
-	Ytrun = c(y[y != 0], rep(0, Ntrun[2]- n[2]));
-	rankdata = sum(Ntrun) + 1 - rank(c(Xtrun, Ytrun));
-	r = sum(rankdata[1:Ntrun[1]]);
-	s = r - Ntrun[1]*(sum(Ntrun) + 1)/2;
-
-	v1 = pmean*(1 - pmean);
-	var1 = N[1]*N[2]*pmean*v1*(sum(N)*pmean + 3*(1 - pmean)/2) + sum(N^2)*v1^2*5/4 + 2*sqrt(2/pi)*pmean*(sum(N)*v1)^(1.5)*sqrt(N[1]*N[2]);
-	var1 = var1/4;
-	var2 = N[1]*N[2]*pmean^2*(sum(N)*pmean + 1)/12;
-	vars = var1 + var2;
-	w = s/sqrt(vars);
-
-	if(perm == FALSE) {p = 2*(1 - pnorm(abs(w)));}
-	else{
-		numrep = 10000; permu.w = rep(0, numrep)
-		Z = c(x, y);
-		for(i in 1:numrep){
-			ind = sample(sum(N), N[1]);
-			x = Z[ind]; y = Z[-ind];
-			n = c(sum(x != 0), sum(y != 0));
-			prop = n/N; pmax = max(prop); pmean = mean(prop);
-			Ntrun = round(pmax*N);
-			Xtrun = c(x[x != 0], rep(0, Ntrun[1] - n[1]));
-			Ytrun = c(y[y != 0], rep(0, Ntrun[2]- n[2]));
-			rankdata = sum(Ntrun) + 1 - rank(c(Xtrun, Ytrun));
-			r = sum(rankdata[1:Ntrun[1]]);
-			s = r - Ntrun[1]*(sum(Ntrun) + 1)/2;
-
-			v1 = pmean*(1 - pmean);
-			var1 = N[1]*N[2]*pmean*v1*(sum(N)*pmean + 3*(1 - pmean)/2) + sum(N^2)*v1^2*5/4 + 2*sqrt(2/pi)*pmean*(sum(N)*v1)^(1.5)*sqrt(N[1]*N[2]);
-			var1 = var1/4;
-			var2 = N[1]*N[2]*pmean^2*(sum(N)*pmean + 1)/12;
-			vars = var1 + var2;
-			permu.w[i] = s/sqrt(vars);
-		}
-		p = sum(abs(w) < abs(permu.w))/numrep;	
-	}
-	H = (p < alpha);
-
-	result <- list(H = H, p.value = p, statistics=w)
-	return(result)
+ziw <- function(x, y, perm = FALSE) {
+  calculate_ziw_statistic= function(x,y){
+    ## total observations in each vector
+    N <- c(length(x), length(y))
+    ## number of non-zero observations in each vector
+    n <- c(sum(x != 0), sum(y != 0))
+    ## non-zero proportion
+    prop <- n / N 
+    pmax <- max(prop) 
+    pmean <- mean(prop)
+    ## keep only round(pmax * N) observations in each group
+    Ntrun <- round(pmax * N)
+    ## truncate zeros in X and Y
+    Xtrun <- c(x[x != 0], rep(0, Ntrun[1] - n[1]))
+    Ytrun <- c(y[y != 0], rep(0, Ntrun[2] - n[2]))
+    rankdata <- sum(Ntrun) + 1 - rank(c(Xtrun, Ytrun))
+    ## mean of the modified wilcoxon rank sum statistic
+    r <- sum(rankdata[1:Ntrun[1]])
+    s <- r - Ntrun[1] * (sum(Ntrun) + 1) / 2
+    ## variance of the modified wilcoxon rank sum statistic
+    v1   <- pmean * (1 - pmean)
+    var1 <- N[1] * N[2] * pmean * v1 * (sum(N) * pmean + 3 * (1 - pmean) / 2) + 
+      sum(N^2) * v1^2 * 5 / 4 + 2 * sqrt(2 / pi) * pmean * (sum(N) * v1) ^ (1.5) *
+      sqrt(N[1] * N[2])
+    var1 <- var1 / 4
+    var2 <- N[1] * N[2] * pmean ^ 2 * (sum(N) * pmean + 1) / 12
+    vars <- var1 + var2
+    ## modified wilcoxon rank sum statistic
+    w <- s / sqrt(vars)
+    return(w)
+  }
+  
+  ## calclulate the ziw statistic
+  w <- calculate_ziw_statistic(x,y)
+  
+  ## calculate the pvalue
+  if (perm) {
+    numrep = 10000 
+    permu.w = rep(0, numrep)
+    N <- c(length(x), length(y))
+    Z <- c(x, y)
+    for (i in 1:numrep) {
+      set.seed(i)
+      ind = sample(sum(N), N[1])
+      x <- Z[ind] 
+      y <- Z[-ind]
+      permu.w[i] <- calculate_ziw_statistic(x,y) 
+    }
+    p <- sum(abs(w) < abs(permu.w)) / numrep
+  }
+  else{
+    p = 2 * (1 - pnorm(abs(w)))
+  }
+  
+  return(list(p.value = p, statistics = w))
 }
